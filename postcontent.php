5d88793106260c2821fb22dd596d1de399cd0537
Bing Cheng <bcheng33@gmail.com>
2013-09-30 11:26:49 -0700

    v1.0

--- postcontent.php
+++ postcontent.php
@@ -1,22 +1,6 @@
 <?php
 echo 'Hello \n';
-//echo $_POST['content1'] . '\n';
+//echo $_POST . '\n';
 //echo $_POST['content2'] . '\n';
 file_put_contents("myfile.txt", $_POST);
 ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Post added</title>
-</head>
-<body>
-
-<a href="userpage.php">Userpage</a>
-<a href="adminpage.php">Adminpage</a>
-
-
-<h2>Your post has been added/edited.</h2>
-
-</body>
-</html>
